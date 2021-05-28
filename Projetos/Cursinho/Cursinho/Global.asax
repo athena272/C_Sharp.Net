e3d54b37e1759c848988f2577f958dfddbf4e0ee
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-05-28 16:19:46 -0300

    Projeto para cursinho da comunidade

---
+++ Projetos/Cursinho/Cursinho/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cursinho.MvcApplication" Language="C#" %>
---
+++ Projetos/Cursinho/Cursinho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cursinho.Startup))]
+namespace Cursinho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
