57cbf41fdcb2d0957cb39c22bde4d95224c5d4db
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-07-08 15:44:50 -0300

    Estudos em POO feitos na DevMidia

---
+++ Estudos/DevMidia/PilaresPOO/ExemplosFundamentos/ExemplosFundamentos.StructExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemplosFundamentos.StructExample
+{
+    public struct PontoGeometrico
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+    }
+}
---
+++ Estudos/DevMidia/PilaresPOO/ExemplosFundamentos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExemplosFundamentos.StructExample;
+
+namespace ExemplosFundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PontoGeometrico ponto = new PontoGeometrico();
+            ponto.Latitude = "-40.0.0";
+            ponto.Longitude = "-20.0.0";
+            Console.WriteLine("Latitude:" + ponto.Latitude);
+            Console.WriteLine("Longitude:" + ponto.Longitude);
+            Console.ReadLine();
+        }
+    }
+}
